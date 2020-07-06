bf488061afadeeb28d5c5cf22ff33ad25fc8a264
aiturrizaga <aiturrizaga@vallegrande.edu.pe>
2020-07-05 23:26:36 -0500

    Agregando los links de redes sociales y contáctenos

--- assets/js/superstore.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(() => {
-  $("#faceboookButton").attr("href", "http://sisuz.com/");
-  $("#whatsappButton").attr("href", "http://sisuz.com/");
-  $("#instragramButton").attr("href", "http://sisuz.com/");
-});
